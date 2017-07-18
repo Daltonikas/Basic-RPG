ff98e43b8021c98815c15ca3b55a4215b786ec36
Unknown <konradas123@gmail.com>
2017-07-18 21:31:23 +0300

    Added a better slime

--- Game_1/Source/States/Playing_State.cpp
+++ Game_1/Source/States/Playing_State.cpp
@@ -20,15 +20,19 @@ namespace State
         sound.play();*/
 
         //Adding manually frames(needs automation), that need to be in loading state, witch needs to be crated;
-        walkAnimation.addFrame({0, 0, 50, 50}, 0.05);
-        walkAnimation.addFrame({50, 0, 50, 50}, 0.09);
-        walkAnimation.addFrame({100, 0, 50, 50}, 0.05);
-        walkAnimation.addFrame({150, 0, 50, 50}, 0.09);
-        walkAnimation.addFrame({200, 0, 50, 50}, 0.05);
+        //walkAnimation.addFrame({LeftEdge, TopEdge, Width, Height},timeForFrame)
+        walkAnimation.addFrame({0, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({16, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({32, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({48, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({64,0, 16, 16}, 0.09);
+        walkAnimation.addFrame({80, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({96, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({112, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({128, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({144, 0, 16, 16}, 0.09);
+        walkAnimation.addFrame({160, 0, 16, 16}, 0.09);
 
-        walkAnimation.addFrame({150, 0, 50, 50}, 0.09);
-        walkAnimation.addFrame({100, 0, 50, 50}, 0.05);
-        walkAnimation.addFrame({50, 0, 50, 50}, 0.09);
     }
     // no input yet for player interaction
     void Playing::input()

a837b88538a570daa6506eaa8dc1ea72c5e1b5da
Unknown <konradas123@gmail.com>
2017-07-28 19:39:54 +0300

    Added Concepts
    For movement, collision, visibility, and action

--- Game_1/Source/Util/Physics.cpp
+++ Game_1/Source/Util/Physics.cpp
@@ -1,3 +1,65 @@
 #include "Physics.h"
 #include "SFML/System.hpp"
 
+Collision::Collision()
+{
+
+}
+
+bool Collision::CheckForColl()
+{
+    ///Needs to check if to objects interlap
+}
+
+bool Collision::DirColl()
+{
+    ///State machine for determining from witch direction collided
+}
+
+void Collision::ResColl()
+{
+    ///Needs to resolve collision: move objects that they would not interlap, etc...
+}
+
+
+Movement::Movement()
+{
+
+}
+
+void Movement::Move()
+{
+    ///Firstly checks where he is ordered to go, by checking what keys where pressed, which he get from whomever call it
+    ///needs to check if nothing is in your way
+    ///then move the sprite over the speed multiplied over the time it was since the las update
+    ///All that need an object
+}
+
+void Movement::Dash()
+{
+    ///Like Move just reacts with the environment
+    ///Maybe knocks enemies back, or goes throw them
+    ///Maybe invulnerable or just with reduced damaged taken
+}
+
+
+
+Action::Action()
+{
+    ///for specific action like attack
+}
+
+void Action::Attack()
+{
+    ///Needs direction and type, amount of damage
+    ///to check if player hits something and then subtract the hp of the enemy
+    ///and terminate if reached 0 hp
+    ///i guess should work with NPC and player
+}
+
+Visibility::Visibility()
+{
+    ///If we will add sneaking and hiding + invisibility
+}
+
+
--- Game_1/Source/Util/Physics.h
+++ Game_1/Source/Util/Physics.h
@@ -2,12 +2,48 @@
 #define PHYSICS_H_INCLUDED
 #include "SFML/System.hpp"
 
-namespace Physics
+class Collision
 {
+public:
+    Collision();
+    bool CheckForColl();
+    bool DirColl();
+    void ResColl();
 
+private:
 
+};
 
-}
+class Movement
+{
+public:
+    Movement();
+    void Move();
+    void Dash();
+    ///At some point we can add sneak
+
+private:
+
+};
+
+class Action
+{
+public:
+    Action();
+    void Attack();
+
+private:
+
+};
+class Visibility
+{
+public:
+    Visibility();
+
+private:
+
+};
+
 
 
 #endif // PHYSICS_H_INCLUDED
